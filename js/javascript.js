cfae48c8d0747cb87a4b52b209c39d038e10320c
baermowei <1255228914@qq.com>
2015-12-10 13:09:45 +0800

    增加了canvas字体
    1,对字体的排版,还真有点糊涂,特别是在我当前的布局下,不过最后终于解决了,当用margin: 100px
    auto的时候整个画布都跟着往下去了---》后来用padding-top解决居中问题
    2,canvas是基于坐标绘制字体的,调整位置和大小也挺纠结的
    3,给字体应用渐变(我真的不懂坐标是怎么个定义的感觉还要加上padding值,反正胡乱调出来的);有空要好好研究研究
    4,两个动画共同用了一个timer,额而且效果有些生硬

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,32 @@
 			
 			var timer=null;
+			var TF=true;
 			window.onload=function(){
 		var canvas=document.getElementById('canvas1');
+		var canvas_two=document.getElementById('canvas2')
 			canvas.width=1400;
 			canvas.height=800;
+			canvas_two.width=800;
+			canvas_two.height=400;
+
+			
 			if (canvas.getContext('2d')) {
 				clearInterval(timer);
-				var context=canvas.getContext('2d');
+			var context=canvas.getContext('2d');
+			var context2=canvas_two.getContext('2d');
+			// context2.font='900 160px 微软雅黑'
+			 // var jianbian=context2.createLinearGradient(100,90,200,500);
+    //     		jianbian.addColorStop(0,"black");
+    //     		jianbian.addColorStop(0.5,"white")
+    //     		context2.fillStyle=jianbian;
+				// context2.fillText("CANVAS",60,250);
+
 				timer=setInterval(function(){
-				rander(context)
+					TF=!TF;
+					rander2(context2);
+					rander(context);
 				}, 500);
+
 			} else {
 			alert('你的浏览器不支持canvas,建议使用高版本浏览器试试')
 					}
@@ -28,3 +45,20 @@
 					cxt.fill();
 				}
 			}
+		function rander2(cxt2){
+			cxt2.clearRect(0,0,800,400);
+				cxt2.font='900 160px 微软雅黑'
+			 var jianbian=cxt2.createLinearGradient(150,100,300,400);
+			 if (TF) {
+			 	jianbian.addColorStop(0,"black");
+        		jianbian.addColorStop(0.5,"white");
+        		cxt2.fillStyle=jianbian;
+			 } else {
+			 	jianbian.addColorStop(0,"white");
+        		jianbian.addColorStop(0.5,"black")
+        		cxt2.fillStyle=jianbian;
+			 }
+        		
+        		
+				cxt2.fillText("CANVAS",60,250);
+		}
